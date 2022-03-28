20a20b8cf07772081ee8aa5780ad971ed88c9208
AloyLooWJ <97747020+AloyLooWJ@users.noreply.github.com>
2022-03-28 12:56:23 +0800

    Create Loan.java

---
+++ Q1/Loan.java
@@ -0,0 +1,74 @@
+import java.time.LocalDateTime;
+
+public class Loan {
+    private static double annualInterestRate;
+    private static int numberOfYears;
+    private static double loanAmount;
+    private static LocalDateTime loanDate;
+
+    public Loan()
+    {
+        setAnnualInterestRate(2.5);
+        setNumberOfYears(1);
+        setLoanAmount(1000);
+    }
+
+    public Loan(double annualIR, int noOfYears, double lAmount)
+    {
+        setAnnualInterestRate(annualIR);
+        setNumberOfYears(noOfYears);
+        setLoanAmount(lAmount);
+    }
+
+    public double getAnnualInterestRate()
+    {
+        return annualInterestRate;
+    }
+
+    public int getNumberOfYears()
+    {
+        return numberOfYears;
+    }
+
+    public double getLoanAmount()
+    {
+        return loanAmount;
+    }
+
+    public LocalDateTime getLoanDate()
+    {
+        loanDate = LocalDateTime.now();
+        System.out.println(loanDate); //2022-02-27T20:20:39.880782500
+        return loanDate;
+    }
+
+    public void setAnnualInterestRate(double new_annualInterestRate)
+    {
+        annualInterestRate = new_annualInterestRate;
+    }
+
+    public void setNumberOfYears(int new_numberOfYears)
+    {
+        numberOfYears = new_numberOfYears;
+    }
+
+    public void setLoanAmount(double new_loanAmount)
+    {
+        loanAmount = new_loanAmount;
+    }
+
+    public double getMonthlyPayment()
+    {
+        double monthlyInterestRate = annualInterestRate/12/100;
+        double a = 1 + monthlyInterestRate;
+        double b = numberOfYears * 12;
+        double monthlyPayment = (loanAmount * monthlyInterestRate)/(1-(1/Math.pow(a,b)));
+        return monthlyPayment;
+    }
+
+    public double getTotalPayment()
+    {
+        double totalPayment = getMonthlyPayment() * numberOfYears * 12;
+        return totalPayment;
+    }
+}
